d72f9a0cc70c74ae17dd69c140c0321183aad1c1
Akash Gaikwad <akashgaikwad211999@gmail.com>
2024-07-05 23:09:30 +0530

    Update app.component.ts

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -54,7 +54,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: 'app-register',
+   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
